431bba90109da26dfbccefa03e7ff0028b862f76
LeuAlmeida <leo@webid.net.br>
2020-02-12 10:39:11 -0300

    feat: create portfolio js file

---
+++ filter/src/portfolio.js
@@ -0,0 +1,26 @@
+export const portfolio = [
+  {
+    name: 'BTM Performance',
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    name: 'October',
+    category: ['all', 'mobile', 'ux-ui'],
+  },
+  {
+    name: 'Driver',
+    category: ['all', 'frontend'],
+  },
+  {
+    name: 'Project',
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    name: 'Marketing',
+    category: ['all', 'others'],
+  },
+  {
+    name: 'Peixoto',
+    category: ['all', 'others'],
+  },
+];
