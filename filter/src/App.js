ed4bc834497d3e146239c5678c8c6eec93248730
LeuAlmeida <leo@webid.net.br>
2020-02-20 11:13:58 -0300

    refactor(app): improving portfolio array into the app file

--- filter/src/App.js
+++ filter/src/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
+const portfolio = [
+  {
+    name: 'BTM Performance',
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    name: 'October',
+    category: ['all', 'mobile', 'ux-ui'],
+  },
+  {
+    name: 'Driver',
+    category: ['all', 'frontend'],
+  },
+  {
+    name: 'Project',
+    category: ['all', 'frontend', 'ux-ui'],
+  },
+  {
+    name: 'Marketing',
+    category: ['all', 'others'],
+  },
+  {
+    name: 'Peixoto',
+    category: ['all', 'others'],
+  },
+];
+
+
 function App() {
   return (
     <h1>Hello World</h1>
